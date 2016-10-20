affed7c027a1d2b4a2b01d23c04472c0db9a30ba
Aleks <aleksz295@gmail.com>
2016-10-20 20:13:14 +1100

    Crash fixed related to getting location after camera

--- app/src/main/java/com/sdstf/info_go/PictureDetailFragment.java
+++ app/src/main/java/com/sdstf/info_go/PictureDetailFragment.java
@@ -6,6 +6,7 @@ import android.graphics.Bitmap;
 import android.location.Location;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,6 +15,7 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
 import com.google.android.gms.location.LocationServices;
@@ -36,7 +38,7 @@ import static android.app.Activity.RESULT_OK;
  * in two-pane mode (on tablets) or a {@link ItemDetailActivity}
  * on handsets.
  */
-public class PictureDetailFragment extends Fragment implements ConnectionCallbacks {
+public class PictureDetailFragment extends Fragment implements ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
     /**
      * The fragment argument representing the item ID that this fragment
      * represents.
@@ -126,12 +128,8 @@ public class PictureDetailFragment extends Fragment implements ConnectionCallbac
             // text desc
             Bundle extras = data.getExtras();
             Bitmap imageBitmap = (Bitmap) extras.get("data");
-            try {
-                mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
-            }
-            catch (SecurityException se) {
-                System.out.println("Please grant permission for location services!");
-            }
+
+            System.out.println(mGoogleApiClient.isConnected());
             addMarker(mLastLocation, imageBitmap);
         }
     }
@@ -156,6 +154,7 @@ public class PictureDetailFragment extends Fragment implements ConnectionCallbac
         if(mGoogleApiClient == null) {
             mGoogleApiClient = new GoogleApiClient.Builder(getActivity())
                     .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
                     .addApi(LocationServices.API)
                     .build();
         }
@@ -204,5 +203,10 @@ public class PictureDetailFragment extends Fragment implements ConnectionCallbac
     @Override
     public void onConnectionSuspended(int i) {
 
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        System.out.println("FAIL");
     }
 }
